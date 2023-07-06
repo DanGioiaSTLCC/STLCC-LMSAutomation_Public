2890e2437e5d6d334ae9a59bd01db6178fa80ff2
Dan Gioia <DanGioiaSTLCC@users.noreply.github.com>
2023-07-06 11:18:05 -0500

    terminal updates

--- MsTerminal-CanvasRunner.ps1
+++ MsTerminal-CanvasRunner.ps1
@@ -1,8 +1,21 @@
-import-module ~\ProfilePathTo\Canvas-API-Lib.ps1
-$VerbosePreference = "Continue"
-$global:CanvasSite = "school.instructure.com"
+import-module ~\dev\BbExtracts\scripts\Canvas-API-Lib.ps1
+import-module ~\dev\BbExtracts\scripts\Canvas-API-Tasks.ps1
+$beta = "school.beta.instructure.com"
+$prod = "school.instructure.com"
+$global:CanvasSite = $prod
 $host.ui.rawui.WindowTitle = "Canvas Runner"
 [string]$ConsoleLoggerName = "{1}\logs\CanvasRunnerLog-{0}.log" -f (Get-Date -uFormat %Y-%m-%d_%H.%M),$env:systemdrive
+
+$tknPath = "~\Apps\Canvas\CanvasAdmin"
+$tknSis = "~\Apps\Canvas\SisAutomations"
+
+$tplResources = "sis_course_id:TPL-Resources"
+$tplCommon = "sis_course_id:TPL-Common"
+
+function Set-EnvVar {
+    $env:CNVSITE=$($global:CanvasSite)
+}
+New-Alias -Name 'Set-PoshContext' -Value 'Set-EnvVar' -Scope Global -Force
 Start-Transcript -Path $ConsoleLoggerName -NoClobber
-$tknPath = "~\PathTo\CanvasTokenFile"
-Clear-Host
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\material.cnv.json" | Invoke-Expression
+$VerbosePreference = "Continue"
